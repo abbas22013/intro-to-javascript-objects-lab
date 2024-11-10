b4734f97444dc74b38364b0940f0d041d436f553
abbas <abbas22013@gmail.com>
2024-11-10 22:53:58 +0300

    abbas

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const game = {
       { name: "rare candy", quantity: 99 },
     ],
     difficulty: "Med"
+    
    
     
   }
@@ -221,6 +222,38 @@ Solve Exercise 10 here:
 game.catchPokemon(pokemon[0]);
 console.log(game.party);
 */
+/*const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: true, difficulty: 3 },
+    { location: "Celadon City", completed: true, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: true, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: true, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+  difficulty: "Med",
+
+
+  catchPokemon: function(pokemonObj) {
+    this.party.push(pokemonObj); 
+  }
+};
+
+const pikachu = { name: "Pikachu", type: "Electric", level: 5 };
+
+game.catchPokemon(pikachu);
+
+console.log(game.party);*/
+
+
 
 
 
@@ -238,7 +271,43 @@ Also, log the `game.items` array to confirm that the pokeball quantity is being 
 Solve Exercise 11 here:
 */
 
-    
+/*const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: true, difficulty: 3 },
+    { location: "Celadon City", completed: true, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: true, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: true, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+  difficulty: "Med",
+
+  catchPokemon: function(pokemonObj) {
+    this.party.push(pokemonObj);
+
+    const pokeball = this.items.find(item => item.name === "pokeball");
+    if (pokeball) {
+      pokeball.quantity--; 
+    }
+  }
+};
+
+const pikachu = { name: "Pikachu", type: "Electric", level: 5 };
+
+game.catchPokemon(pikachu);
+console.log(game.party);   
+console.log(game.items);   */
+
+
+
 
 
 
@@ -258,6 +327,69 @@ game.gyms.forEach(gym => {
 });
 
 console.log(game.gyms);
+
+
+/*
+Exercise 13
+1. Create a `gymStatus` method in `game` to tally completed and incomplete gyms.
+2. How will you iterate through the `gyms` array and update the tally? Remember to log the final tally.
+
+This method should:
+  - Not accept any arguments.
+  - Initially create a constant `gymTally`, which is an object that has two 
+    properties: `completed` and `incomplete`, both of which are initially set to 0.
+  - Iterate through the objects in the `game.gyms` array and update the 
+    properties on `gymTally` as follows: 
+    - `completed` should count how many gyms in the array have a value of `true` 
+      for their `completed` property. 
+    - `incomplete` should count how many gyms in the array have a value of 
+      `false` for their `completed` property.
+  - Log the value of `gymTally`.
+  - The method should not return anything.
+
+For example, if five gym objects have a value of `true` on their `completed` property and three gym objects have a value of `false` on their `completed` property, the logged value would be: `{ completed: 5, incomplete: 3 }`.
+
+Solve Exercise 13 here:
+*/
+/*const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: true, difficulty: 3 },
+    { location: "Celadon City", completed: true, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: true, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: true, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+  difficulty: "Med",
+
+  gymStatus: function() {
+    const gymTally = {
+      completed: 0,
+      incomplete: 0
+    };
+
+
+    this.gyms.forEach(gym => {
+      if (gym.completed) {
+        gymTally.completed++;
+      } else {
+        gymTally.incomplete++;
+      }
+    });
+
+    console.log(gymTally);
+  }
+};
+
+game.gymStatus();*/
 
 
 
